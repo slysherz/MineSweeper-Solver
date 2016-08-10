d46a459281489a9931182a936b8cc6ee1b5c3675
SlySherZ <the.lord.skull@hotmail.com>
2016-08-10 23:48:07 +0100

    Create group_list.h

---
+++ group_list.h
@@ -0,0 +1,11 @@
+// Simple way to have a list with all the current groups
+Group groupList[GSIZE];
+
+// Current size of the list
+int groupListArrayNumber = 0;
+
+// Adds 'group' to the list
+void addToList (Group group);
+
+// Removes the group with index 'index' from the group
+void removeFromList (int index);
