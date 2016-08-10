174cf1251936315c2e8761d11dcdef946e9e4fce
SlySherZ <the.lord.skull@hotmail.com>
2016-08-10 23:45:24 +0100

    Update bigFile.c

--- bigFile.c
+++ bigFile.c
@@ -71,26 +71,6 @@ int makePlayAround (
 
 /****************************** AUTOMATED SOLVER ******************************/
 // GROUPS
-// A group is a list with 'size' cells, which contains 'mines' mines
-typedef struct {
-	int mines;
-	int positions[8][2];
-	int size;
-} Group ;
-
-void printGroup (Group group);
-
-// Is cell (x, y) contained in group?
-bool cellContainedIn (int x, int y, Group group);
-
-// Is group a contained in group b?
-bool containedIn (Group a, Group b);
-
-// Removes all 'group' cells from 'from'
-Group subtract (Group from, Group group);
-
-// Returns the amount of cells 'a' has in common with 'b'
-int intersect(Group a, Group b);
 
 // GROUP LIST
 // Simple way to have a list with all the current groups
