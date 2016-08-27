ce5fa4b538e25fc495950a5ee8ccde5bd5ccf95a
KuroLevin <pedromoreno94@gmail.com>
2016-08-27 22:03:07 +0100

    seg fault :'(

--- solver.c
+++ solver.c
@@ -24,27 +24,10 @@ int solveMineSweeper(Board boardh, Board boardv) {
 
 	while (1) {
 MAYBE(printf("creation of groups complete\n"));	
-		Group *pi = groupHead, *pj; 
-
-		while (pi!=NULL){
-//MAYBE(printf("1st while\n"));
-			pj=pi->next;
-			while(pj!=NULL){
-//MAYBE(printf("2nd while\n"));
-				if(sanitaize(pi, pj)){
-					pi=groupHead;
-					break;
-				}
-				else{
-					pj=pj->next;
-				}
-//MAYBE(printGroupList());			
-			}	
-			pi=pi->next;
-		}
 
 
 
+		cleanEtAll(groupHead);
 
 MAYBE(printf("cleaning of groups complete\n"));
 		bool stallAlert = 0;
@@ -57,7 +40,23 @@ MAYBE(printf("cleaning of groups complete\n"));
 				for (int  j =0; j < ptr->size*2; j+=2) {
 					makePlay(boardh, boardv, ptr->positions[j], ptr->positions[j+1],CLEAR);
 MAYBE(printf("Marking Clear (%d,%d)\n", ptr->positions[j], ptr->positions[j+1]));
+										
+					Group *newGroup = buildGroup(boardv, ptr->positions[j], ptr->positions[j+1]); 
+					if (newGroup->size > 0) {
+						addToList(newGroup);
+						cleanEtAll(newGroup);
+					}
+					else {
+						freeGroup(newGroup);
+					}
+
+
+
+
 				}
+				ptr=removeFromList(ptr);
+				stallAlert=1;
+				continue;
 			} 
 			else if (ptr->mines == ptr->size) {
 				for (int  j = 0; j < ptr->size*2; j+=2) {
@@ -71,37 +70,9 @@ MAYBE(printf("Marking Mine (%d,%d)\n", ptr->positions[j], ptr->positions[j+1]));
 				continue;
 //MAYBE(printGroupList());
 			} 
-			else {
-				ptr=ptr->next;
-				continue;
-			}
-			
-			// Delete group
-			stallAlert = 1;
-			// maybe break;
 			ptr=ptr->next;
-		}
-		
-
-		ptr=groupHead;
-		while(ptr!=NULL){
-
-			int result = NEUTRAL;
-			if (ptr->mines == 0) {
-				for (int  j =0; j < ptr->size*2; j+=2) {
-					Group *newGroup = buildGroup(boardv, ptr->positions[j], ptr->positions[j+1]); 
-					if (newGroup->size > 0) {
-						addToList(newGroup);
-					}
-					else {
-						freeGroup(newGroup);
-					}
-				}
-				// Delete group
-				ptr=removeFromList(ptr);
-				continue;
-			}
-			ptr=ptr->next;	
+			
+			
 		}
 		
 		printBoard(boardv);
@@ -150,11 +121,14 @@ bool clean(Group *pi, Group *pj){
 	else if (containedIn(pi, pj)) { 
 					//remove elements of i from j
 		subtract(pj, pi);
+		cleanEtAll(pj);
 	}
 	else if (containedIn(pj, pi)) {
 					//remove elements of j from i
 					
 		subtract(pi, pj);
+		cleanEtAll(pi);
+		return 1;
 	}
 	else if (intersectSolver(pi, pj)>=0) {
 		int x = intersectSolver(pi, pj);
@@ -162,16 +136,19 @@ bool clean(Group *pi, Group *pj){
 		Group *a;
 		a = groupCopy(pi);
 		subtract(pi, pj);	// LEFT
+		pi->mines = pi->mines + pj->mines - x;
+		cleanEtAll(pi);
+
 		subtract(a, pi);  //isto vai dar merda!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		a->mines = x;
 		addToList(a);
+		cleanEtAll(a);
+
 		subtract(pj, a);
-		pi->mines = pi->mines + pj->mines;
+		cleanEtAll(pj);
+
+		return 1;
 	}
-	else {
-		return 0;
-	}
-	return 1;
+	return 0;
 }
 
 void cleanEtAll(Group *group){
@@ -180,7 +157,9 @@ void cleanEtAll(Group *group){
 		if(ptr==group){
 			ptr=ptr->next;
 		}
-		clean(group, ptr);
+		if(clean(group, ptr)){
+			break;
+		}
 		ptr=ptr->next;
 	}
 }
